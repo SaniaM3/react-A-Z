d17d05fb02c485bc3f73755808cf28af689c6f29
Aleksandr Masko <amasko18@gmail.com>
2022-03-30 00:20:11 +0300

    commit

--- src/components/PostItem/PostItem.jsx
+++ src/components/PostItem/PostItem.jsx
@@ -1,3 +1,4 @@
+import MyButton from "../UI/button/MyButton";
 
 
 
@@ -6,13 +7,13 @@ const PostItem = (props) => {
     return (
         <div className='post'>
                 <div className='post__content'>
-                    <strong>{props.post.id}.{props.post.title}</strong>
+                    <strong>{props.number}.{props.post.title}</strong>
                     <div>
                         {props.post.body}
                     </div>
                 </div>
                 <div className='post__btns'>
-                    <button>Delete</button>
+                    <MyButton onClick={() => props.remove(props.post)}>Delete</MyButton>
                 </div>
             </div>
     )

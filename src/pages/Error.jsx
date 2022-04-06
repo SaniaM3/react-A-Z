108756f288625add4031e7406dfae8850783fb04
Aleksandr Masko <amasko18@gmail.com>
2022-04-07 00:13:04 +0300

    commit

---
+++ src/pages/Error.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Error = () => {
+    return (
+        <div>
+            <h1 style={{color:'red', textAlign:'center'}}> 404</h1>
+            <h1 style={{textTransform:'uppercase', color:'black', textAlign:'center'}}>not found</h1>
+        </div>
+    );
+};
+
+export default Error;
